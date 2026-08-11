a8927a5c4173faf306eebaea63978a56fb60e344
Dong <dongchu2003@gmail.com>
2026-08-11 10:55:47 +0700

    init(registration-service): registration with cross-service call

--- registration-service/registration-service/src/main/java/vn/edu/crs/registration_service/config/RestTemplateConfig.java
+++ registration-service/registration-service/src/main/java/vn/edu/crs/registration_service/config/RestTemplateConfig.java
@@ -1,4 +1,16 @@
 package vn.edu.crs.registration_service.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
+@Configuration
 public class RestTemplateConfig {
-}
+
+        @Bean
+        public RestTemplate restTemplate() {
+            // Dùng JdkClientHttpRequestFactory để hỗ trợ phương thức PATCH
+            return new RestTemplate(new JdkClientHttpRequestFactory());
+        }
+    }
+
